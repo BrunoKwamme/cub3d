eac02647ccbae28e4ad6305db8379a88ba3d06df
Bruno <ambsouza@gmail.com>
2024-11-26 13:59:37 -0300

    assets folder and mlx practices

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -9,4 +9,21 @@
 # include "gnl/includes/get_next_line.h"
 # include "../minilibx/mlx.h"
 
+
+typedef struct	s_data {
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		x;
+	int		y;
+	int		color;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+}				t_data;
+
 #endif
