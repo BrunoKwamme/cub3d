1c14c77b71671f49528e7ce6230ed90b0ccd6a51
bkwamme <ambsouza@gmail.com>
2025-04-17 20:54:56 -0300

    start map renderization

--- srcs/main.c
+++ srcs/main.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabrfern <gabrfern@student.42.rio>         +#+  +:+       +#+        */
+/*   By: bkwamme <bkwamme@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:47:11 by bkwamme           #+#    #+#             */
-/*   Updated: 2025/03/20 01:22:12 by gabrfern         ###   ########.fr       */
+/*   Updated: 2025/04/17 20:53:27 by bkwamme          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
-/*
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -28,34 +28,67 @@ int	close_window(int keycode, t_data *img)
 	return (0);
 }
 
-void start_mlx ()
+void convert_arr_in_map(t_map **map, t_data *img)
+{
+	int	i_arr;
+	int i;
+	int j = 0;
+	int k = 0;
+
+	i_arr = 0;
+	while ((*map)->map_layout[i_arr] != NULL)
+	{
+		j = 0;
+		i = 0;
+		while ((*map)->map_layout[i_arr][i] != LIMIT_INT_STD)
+		{
+			if((*map)->map_layout[i_arr][i] < 10 && (*map)->map_layout[i_arr][i] > -1)
+			{
+				if((*map)->map_layout[i_arr][i] == 1)
+					mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, j,k);
+				j = j + 32;
+			}
+			i++;
+		}
+		k = k + 32;
+		i_arr++;
+	}
+}
+
+void start_mlx (t_map **map)
 {
 	t_data	img;
 
-	img.x = 5;
-	img.y = 5;
+	img.x = 31;
+	img.y = 31;
 	img.relative_path = "./assets/grass.xpm";
 	printf("relative path -> %s\n", img.relative_path);
 	img.color = 0x00FF0000;
 	img.mlx = mlx_init();
 	img.img = mlx_xpm_file_to_image(img.mlx, img.relative_path, &img.img_width, &img.img_height);
 	//img.img = mlx_new_image(img.mlx, 640, 480);
-	img.mlx_win = mlx_new_window(img.mlx, 640, 480, "Hello, World!");
+	img.mlx_win = mlx_new_window(img.mlx, 1920, 1080, "Hello, World!");
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	my_mlx_pixel_put(&img, img.x, img.y, img.color);
-	mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+	convert_arr_in_map(map, &img);
+	//mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 40, 0);
 	mlx_hook(img.mlx_win, 2, 1L<<0, close_window, &img);
 	mlx_loop(img.mlx);
-	return (0);
-} */
+}
+
 
 int	main(int argc, char **argv)
 {
 	t_map	*map;
+	//t_data img;
 
+	//img.mlx = mlx_init();
+	//img.img = mlx_new_image(img.mlx, 1920, 1080);
+	//img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	if (argc != 2 || argument_val(argv) != 0)
 		return (put_error("BAD INPUT"), 2);
 	map = read_document(argv[1]);
+	start_mlx(&map);
 	if (map != NULL)
 	{
 		printf("north -> %s\nsouth ->%s\nweast -> %s\neast -> %s\n", map->no_texture, map->so_texture, map->we_texture, map->ea_texture);
